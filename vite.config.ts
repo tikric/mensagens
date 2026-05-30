b9a7e4633525a9d179422d9174e8656fa66e3689
tikric <pl.economica@gmail.com>
2026-05-30 12:50:56 -0300

    iiityut

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: process.env.NODE_ENV === 'production' ? '/mensagens/' : '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
